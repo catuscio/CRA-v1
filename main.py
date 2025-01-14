151aa705f927764177a486c6882f45a1cbada7bb
catuscio <77739745+catuscio@users.noreply.github.com>
2025-01-15 08:43:33 +0900

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from google.oauth2 import service_account
-from google.cloud import storage
 
 # Create API client.
 credentials = service_account.Credentials.from_service_account_info(
