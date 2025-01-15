3c51d932de789570d7a22503b9239168e7844783
catuscio <77739745+catuscio@users.noreply.github.com>
2025-01-15 17:04:13 +0900

    Update main.py

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ from google.oauth2 import service_account
 credentials = service_account.Credentials.from_service_account_info(
     st.secrets["gcp_service_account"]
 )
+print(credentials)
 
 #---------------------------------#
 #---------- UI Settings ----------#
