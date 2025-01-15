bb4a2bf437207fdd7880899d8322935b2e450665
catuscio <77739745+catuscio@users.noreply.github.com>
2025-01-15 18:25:59 +0900

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,6 @@ load_dotenv()
 # Create API client.
 credentials = service_account.Credentials.from_service_account_info(
     st.secrets["gcp_service_account"],
-    scopes=['https://www.googleapis.com/auth/cloud-platform']
 )
 
 # Gemini 구성
