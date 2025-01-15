bf693c4a396384a548e0079531f6eb76c34788d9
catuscio <77739745+catuscio@users.noreply.github.com>
2025-01-15 18:13:57 +0900

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,10 @@ credentials = service_account.Credentials.from_service_account_info(
 )
 
 # Gemini 구성
-genai.configure(credentials=credentials)
+genai.configure(
+    credentials=credentials,
+    project="gen-lang-client-0009382783"
+)
 
 #---------------------------------#
 #---------- UI Settings ----------#
